5caaf90a963f55609eb25b663b895396ccc1c091
Naveen5699 <naveenkumar561999@gmail.com>
2022-11-18 23:08:58 +0530

    naveen

---
+++ src/org/test/NaveTestProject.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class NaveTestProject {
+	
+	
+	private void n1() {
+		
+		System.out.println("naveen");
+
+	}
+public static void main(String[] args) {
+	System.out.println("nav");
+}
+}
